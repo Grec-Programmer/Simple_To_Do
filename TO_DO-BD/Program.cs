7ba90728598a77b51ad9812a2a3f62485366052e
Master <daniil.grechko.00@mail.ru>
2023-04-01 04:27:02 +0300

    Просмотр списка дел

---
+++ TO_DO-BD/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TO_DO_BD
+{
+    class Program
+    {
+        
+        SqlConnection con;
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader reader;
+        string cs = ConfigurationManager.ConnectionStrings["ToDo"].ConnectionString;
+        public Program()
+        {
+            con = new SqlConnection(cs);
+        }
+        public void WatchTask()
+        {
+            try
+            {
+                int step = 0;
+                con.Open();
+                string com = "Select * FROM ToDo";
+                cmd = new SqlCommand(com, con);
+                reader = cmd.ExecuteReader();
+                int line = 0;
+                while (reader.Read())
+                {
+                    if (line == 0)
+                    {
+                        for (int i = 1; i < reader.FieldCount; i++)
+                        {
+                            Console.Write($"\t{reader.GetName(i)}\t|\t");
+                        }
+                    }
+                    Console.WriteLine();
+                    line++;
+                    Console.Write($"\t{reader[1]} \t {reader[2]}\n");
+                    Console.SetCursorPosition(60, step += 2);
+                    Console.Write(reader[3]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Что-то пошло не так");
+            }
+            finally { con.Close(); }
+        }
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.WatchTask();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ TO_DO-BD/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TO_DO-BD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TO_DO-BD")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9a54df1d-68ab-4f19-bb0d-51e3294f0747")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
